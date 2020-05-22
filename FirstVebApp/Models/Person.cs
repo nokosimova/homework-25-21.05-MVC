3b3e6bcde51d53e098fc70089a5811a895514e88
kosimovanodira <nkosimova@gmail.com>
2020-05-22 22:17:20 +0500

    create project, add Person model

---
+++ FirstVebApp/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstVebApp.Models
+{
+    public class Person
+    {
+        int id { get; set; }
+        string LastName { get; set; }
+        string FirstName { get; set; }
+        string MiddleName { get; set; }
+
+    }
+}
